9a5e4fb75a1fdbf8aa768531a064d4cad5dc0c67
Mikhail Karpov <mihanikk@gmail.com>
2019-02-17 17:20:39 +0300

    Updates

---
+++ precache-manifest.9fd34d3d9aabb228cc9a48ab9f0512d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ce07be25999d95cba96",
+    "url": "/news-react-app-frontend/static/js/runtime~main.3ce07be2.js"
+  },
+  {
+    "revision": "4ec5faadc1db961c412c",
+    "url": "/news-react-app-frontend/static/js/main.4ec5faad.chunk.js"
+  },
+  {
+    "revision": "d1bf4d310c274c82171e",
+    "url": "/news-react-app-frontend/static/js/2.d1bf4d31.chunk.js"
+  },
+  {
+    "revision": "4ec5faadc1db961c412c",
+    "url": "/news-react-app-frontend/static/css/main.1d9e6397.chunk.css"
+  },
+  {
+    "revision": "e2fd28deb05cf53d6e2e6283d72625fb",
+    "url": "/news-react-app-frontend/index.html"
+  }
+];
